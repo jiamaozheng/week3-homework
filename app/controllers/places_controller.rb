d39c4c7f72034e6189d288dd3d0fb9fbf047d436
Jiamao Zheng <jiamaoz@yahoo.com>
2015-07-13 00:14:27 -0500

    finish challenge 4

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,9 +9,35 @@ class PlacesController < ApplicationController
   end
 
   def delete
-    place_to_be_deleted = Place.find_by(:title => params["id"])
+    place_to_be_deleted = Place.find_by(:id => params["id"])
     place_to_be_deleted.delete
-    redirect_to '/'
+    redirect_to '/places'
   end
 
+  def new 
+    render "new"
+  end
+
+  def create
+    title = params["title"]
+    url = params["photo_url"]
+    price = params["admission_price"]
+    description = params["description"]
+    Place.create(title: title, photo_url: url, admission_price: price, description: description)
+    redirect_to "/places"
+  end
+
+  def edit
+    @place_to_be_edited = Place.find_by(:title => params["id"])
+  end
+
+  def update
+    update_place = Place.find_by(:title => params["id"])
+    update_place.title = params["title"]
+    update_place.photo_url = params["url"]
+    update_place.admission_price = params["price"]
+    update_place.description = params["description"]
+    update_place.save
+    redirect_to "/places"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get '/places/new' => 'places#new'
+  get '/places/create' => 'places#create'
+  get '/places/:id/edit' => 'places#edit'
+  get '/places/:id/update' => 'places#update'
+
   root 'places#index'
   get '/places' => 'places#index'
-  get '/places/:id' => 'places#show'
   get '/places/:id/delete' => 'places#delete'
+  get '/places/:id' => 'places#show'
 
 end
